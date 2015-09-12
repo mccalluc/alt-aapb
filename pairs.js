da6f685e8cc374dc86949aea25c94dcf6d8b0803
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-09-12 10:35:22 -0400

    Mocking response.

--- pairs.js
+++ pairs.js
@@ -4,6 +4,20 @@ var terms = decodeURIComponent(location.search.replace('?terms=',''))
   
 d3.select('input').attr('value',terms.join(', '));
 
+var pairs = [];
+
+for (var a=0; a<terms.length; a++) {
+  for (var b=0; b<a; b++) {
+    pairs.push([terms[a], terms[b]].sort().join("-"));
+  }
+}
+
+// MOCK:
+var counts = {};
+pairs.forEach(function(pair){
+  counts[pair] = Math.pow(Math.random(),2) * 100;
+});
+
   d3.select("body").append("svg")
       .attr("width", window.innerWidth)
       .attr("height", window.innerHeight)
@@ -11,20 +25,21 @@ d3.select('input').attr('value',terms.join(', '));
 
   var svg = d3.select("svg");
 
-  var n = 11;
+  var n = terms.length;
   var rotation = Math.PI/2 + Math.PI/n;
   for (var a=0; a<n; a++) {
     var ax = 80 * Math.cos(2*Math.PI*a/n + rotation);
     var ay = 100 * Math.sin(2*Math.PI*a/n + rotation);
     svg.append("text")
        .attr({"x": ax + 20 * Math.sign(ax), "y": ay, "text-anchor": ax > 0 ? "start" : "end" })
-       .text("lorem ipsum "+a);
+       .text(terms[a]);
     for (var b=0; b<a; b++) {
       var bx = 80 * Math.cos(2*Math.PI*b/n + rotation);
       var by = 100 * Math.sin(2*Math.PI*b/n + rotation);
       var sweep = 0;
+      var key = [terms[a],terms[b]].sort().join('-');
       svg.append("path")
         .attr("d", "M "+ax+" "+ay+" A 250 200, 0, 0, "+sweep+", "+bx+" "+by)
-        .attr("style", "stroke-width: "+Math.pow(Math.random(),2)*10);
+        .attr("style", "stroke-width: "+(counts[key])/10);
     }
   }
